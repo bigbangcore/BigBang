e8e6c6768149a384f14298130c2f3d4358073495
cch <cch809094@163.com>
2019-08-16 16:59:27 +0800

    modify extened block spacing to 2 seconds

--- src/bigbang/param.h
+++ src/bigbang/param.h
@@ -20,7 +20,7 @@ static const unsigned int MAX_BLOCK_SIZE = 2000000;
 static const unsigned int MAX_TX_SIZE = (MAX_BLOCK_SIZE / 20);
 
 static const unsigned int BLOCK_TARGET_SPACING = 60; // 1-minute block spacing
-static const unsigned int EXTENDED_BLOCK_SPACING = 1;
+static const unsigned int EXTENDED_BLOCK_SPACING = 2;
 static const unsigned int PROOF_OF_WORK_DECAY_STEP = BLOCK_TARGET_SPACING;
 
 static const unsigned int MINT_MATURITY = 120; // 120 blocks about 2 hours
