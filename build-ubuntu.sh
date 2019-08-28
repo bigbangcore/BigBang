949e221de4625f0399d6af6b6f351ecbe377c285
MathxH Chen <brainfvck@foxmail.com>
2019-08-28 13:44:07 +0800

    Add build script on ubuntu

---
+++ build-ubuntu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get update -qq;
+sudo apt install -y g++ cmake libboost-all-dev openssl libssl1.0-dev libreadline-dev pkg-config libsodium-dev libprotobuf-dev protobuf-compiler;
+./INSTALL.sh
