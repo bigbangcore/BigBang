2de94ad3e361f4c9c56bd0944554d706491ab4ba
MathxH Chen <brainfvck@foxmail.com>
2019-09-10 10:55:16 +0800

    Change log level string from lower-case to upper-case

--- src/xengine/logger.cpp
+++ src/xengine/logger.cpp
@@ -30,10 +30,10 @@ inline std::basic_ostream<CharT, TraitsT>& operator<<(
     std::basic_ostream<CharT, TraitsT>& strm, severity_level lvl)
 {
     static const char* const str[] = {
-        "debug",
-        "info",
-        "warn",
-        "error"
+        "DEBUG",
+        "INFO",
+        "WARN",
+        "ERROR"
     };
     if (static_cast<std::size_t>(lvl) < (sizeof(str) / sizeof(*str)))
         strm << str[lvl];
