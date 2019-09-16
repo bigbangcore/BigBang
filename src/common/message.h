b934951fd585f37625f8d14ae210bc55a90b21c0
MathxH Chen <brainfvck@foxmail.com>
2019-09-16 14:22:50 +0800

    Add Message struct for peer

--- src/common/message.h
+++ src/common/message.h
@@ -5,5 +5,76 @@
 #ifndef COMMON_MESSAGE_H
 #define COMMON_MESSAGE_H
 
+#include <uint256.h>
+#include "message/message.h"
+#include "block.h"
+#include "proto.h"
+
+using namespace xengine;
+using namespace bigbang::network;
+
+struct CPeerBasicMessage : public CMessage
+{
+    GENERATE_MESSAGE_VIRTUAL_FUNCTION(CPeerBasicMessage);
+    uint64 nNonce;
+    uint256 hashFork;
+};
+
+INITIALIZE_MESSAGE_TYPE(CPeerBasicMessage);
+
+struct CPeerSubscribeMessage : public CPeerBasicMessage
+{
+    GENERATE_MESSAGE_VIRTUAL_FUNCTION(CPeerSubscribeMessage);
+    std::vector<uint256> vecForks;
+};
+
+INITIALIZE_MESSAGE_TYPE(CPeerSubscribeMessage);
+
+struct CPeerUnSubscribeMessage : public CPeerSubscribeMessage
+{
+    GENERATE_MESSAGE_VIRTUAL_FUNCTION(CPeerUnSubscribeMessage);
+};
+
+INITIALIZE_MESSAGE_TYPE(CPeerUnSubscribeMessage);
+
+struct CPeerGetBlocksMessage : public CPeerBasicMessage
+{
+    GENERATE_MESSAGE_VIRTUAL_FUNCTION(CPeerGetBlocksMessage);
+    CBlockLocator blockLocator;
+};
+
+INITIALIZE_MESSAGE_TYPE(CPeerGetBlocksMessage);
+
+struct CPeerGetDataMessage : public CPeerBasicMessage
+{
+    GENERATE_MESSAGE_VIRTUAL_FUNCTION(CPeerGetDataMessage);
+    std::vector<CInv> vecInv;
+};
+
+INITIALIZE_MESSAGE_TYPE(CPeerGetDataMessage);
+
+struct CPeerInvMessage : public CPeerBasicMessage
+{
+    GENERATE_MESSAGE_VIRTUAL_FUNCTION(CPeerInvMessage);
+    std::vector<CInv> vecInv;
+};
+
+INITIALIZE_MESSAGE_TYPE(CPeerInvMessage);
+
+struct CPeerTxMessage : public CPeerBasicMessage
+{
+    GENERATE_MESSAGE_VIRTUAL_FUNCTION(CPeerTxMessage);
+    CTransaction tx;
+};
+
+INITIALIZE_MESSAGE_TYPE(CPeerTxMessage);
+
+struct CPeerBlockMessage : public CPeerBasicMessage
+{
+    GENERATE_MESSAGE_VIRTUAL_FUNCTION(CPeerBlockMessage);
+    CBlock block;
+};
+
+INITIALIZE_MESSAGE_TYPE(CPeerBlockMessage);
 
 #endif // COMMON_MESSAGE_H
