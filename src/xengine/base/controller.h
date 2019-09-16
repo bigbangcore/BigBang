f9d3e841c194477a8df394a557260f6c4f6f89fd
Loading Sun <jhkghmn@gmail.com>
2019-09-16 11:47:34 +0800

    Add:  - IModel, virtual base class of model  - IController, virtual base class of controller

---
+++ src/xengine/base/controller.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2019 The Bigbang developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef XENGINE_BASE_CONTROLLER_H
+#define XENGINE_BASE_CONTROLLER_H
+
+#include <string>
+
+#include "base/base.h"
+
+namespace xengine
+{
+
+class IController : public IBase
+{
+public:
+    IController(const std::string& ownKeyIn = "")
+      : IBase(ownKeyIn) {}
+};
+
+} // namespace xengine
+
+#endif // XENGINE_BASE_CONTROLLER_H
---
+++ src/xengine/base/model.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2019 The Bigbang developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef XENGINE_BASE_MODEL_H
+#define XENGINE_BASE_MODEL_H
+
+#include <string>
+
+#include "base/base.h"
+
+namespace xengine
+{
+
+class IModel : public IBase
+{
+public:
+    IModel(const std::string& ownKeyIn = "")
+      : IBase(ownKeyIn) {}
+};
+
+} // namespace xengine
+
+#endif // XENGINE_BASE_MODEL_H
