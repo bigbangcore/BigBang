050b2604895eefced1246919bd6c2fc4f00bcd8b
Loading Sun <jhkghmn@gmail.com>
2019-09-16 11:24:04 +0800

    Create message struct file

---
+++ src/common/message.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2019 The Bigbang developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef COMMON_MESSAGE_H
+#define COMMON_MESSAGE_H
+
+
+#endif // COMMON_MESSAGE_H
