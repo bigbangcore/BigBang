f019ecbfc287b57d33e6d38a790d9ab08b386c29
MathxH Chen <brainfvck@foxmail.com>
2019-09-19 15:15:38 +0800

    Init netchannel unit test

---
+++ test/netchn_tests.cpp
@@ -0,0 +1,21 @@
+// Copyright (c) 2019 The Bigbang developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#include <boost/test/unit_test.hpp>
+
+#include "docker/config.h"
+#include "docker/docker.h"
+#include "message/actor.h"
+#include "message/message.h"
+#include "message/messagecenter.h"
+#include "test_big.h"
+
+using namespace xengine;
+
+BOOST_FIXTURE_TEST_SUITE(netchn_tests, BasicUtfSetup)
+
+BOOST_AUTO_TEST_CASE(netchn_msg)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
