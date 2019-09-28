38491a5ce58a303edbd56f14adad69c474d2eeac
MathxH Chen <brainfvck@foxmail.com>
2019-09-28 14:47:54 +0800

    Init Message for peernet

--- src/common/message.h
+++ src/common/message.h
@@ -165,6 +165,47 @@ struct CPeerPublishMessageOutBound : public CPeerPublishMessageInBound
     GENERATE_MESSAGE_FUNCTION(CPeerPublishMessageOutBound);
 };
 
+////////////////////   PeerNet  ////////////////////////
+struct CPeerNetGetIPMessage : public xengine::CMessage
+{
+    GENERATE_MESSAGE_FUNCTION(CPeerNetGetIPMessage);
+};
+
+struct CPeerNetGetCountMessage : public xengine::CMessage
+{
+    GENERATE_MESSAGE_FUNCTION(CPeerNetGetCountMessage);
+};
+
+struct CPeerNetGetPeersMessage : public xengine::CMessage
+{
+    GENERATE_MESSAGE_FUNCTION(CPeerNetGetPeersMessage);
+};
+
+struct CPeerNetAddNodeMessage : public xengine::CMessage
+{
+    GENERATE_MESSAGE_FUNCTION(CPeerNetAddNodeMessage);
+};
+
+struct CPeerNetRemoveNodeMessage : public xengine::CMessage
+{
+    GENERATE_MESSAGE_FUNCTION(CPeerNetRemoveNodeMessage);
+};
+
+struct CPeerNetGetBannedMessage : public xengine::CMessage
+{
+    GENERATE_MESSAGE_FUNCTION(CPeerNetGetBannedMessage);
+};
+
+struct CPeerNetSetBanMessage : public xengine::CMessage
+{
+    GENERATE_MESSAGE_FUNCTION(CPeerNetSetBanMessage);
+};
+
+struct CPeerNetClrBannedMessage : public xengine::CMessage
+{
+    GENERATE_MESSAGE_FUNCTION(CPeerNetClrBannedMessage);
+};
+
 /// Add an unconfirmed transaction.
 struct CAddTxMessage : public xengine::CMessage
 {
@@ -173,8 +214,6 @@ struct CAddTxMessage : public xengine::CMessage
     uint256 hashFork;
     CTransaction tx;
 };
-
-//////////// Delegate NetChannel //////////
 
 //////////// TxPool ///////////////////
 
