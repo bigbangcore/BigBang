43f7eae8fc302bc00cd3f891baaf2f6f6bcb8ed1
MathxH Chen <brainfvck@foxmail.com>
2019-09-29 17:33:48 +0800

    Add New test case

--- test/network_tests.cpp
+++ test/network_tests.cpp
@@ -1,8 +1,6 @@
 // Copyright (c) 2019 The Bigbang developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-#include "netchn.h"
-
 #include <boost/test/unit_test.hpp>
 #include <chrono>
 #include <thread>
@@ -48,6 +46,15 @@ public:
     {
         DeregisterHandler(CPeerNetCloseMessage::MessageType());
         DeregisterHandler(CPeerNetRewardMessage::MessageType());
+    }
+
+    void AddTestNewPeer()
+    {
+        boost::asio::io_service ioService;
+        CIOProc* pProc = new CIOProc("TestProc");
+        CIOContainer* pContainer = new CIOCachedContainer(pProc);
+        CIOClient* pIoClient = new CSocketClient(pContainer, ioService);
+        AddNewPeer(pIoClient, true);
     }
 
 protected:
@@ -263,8 +270,7 @@ BOOST_AUTO_TEST_CASE(delegated_chn_msg)
 
     BOOST_CHECK(docker.Initialize(pCfg, &log));
     BOOST_CHECK(docker.Attach(new CConsensus()));
-    auto pCoreProtocol = new CCoreProtocol();
-    BOOST_CHECK(docker.Attach(pCoreProtocol));
+    BOOST_CHECK(docker.Attach(new CCoreProtocol()));
     BOOST_CHECK(docker.Attach(new CDummyPeerNet()));
     BOOST_CHECK(docker.Attach(new CForkManager()));
     BOOST_CHECK(docker.Attach(new CWallet()));
@@ -316,6 +322,61 @@ BOOST_AUTO_TEST_CASE(delegated_chn_msg)
     std::this_thread::sleep_for(std::chrono::seconds(1));
 
     BOOST_CHECK(pDelegatedChannel->TestDeactiveNonce(spDeactiveMsg->nNonce));
+
+    docker.Exit();
+
+    boost::filesystem::remove_all(dataPath);
+}
+
+BOOST_AUTO_TEST_CASE(peernet_msg)
+{
+    auto pCfg = new CBasicConfig();
+    boost::filesystem::path dataPath(boost::filesystem::current_path().generic_string() + boost::filesystem::path("/data-dir").generic_string());
+    boost::filesystem::create_directory(dataPath);
+    std::string args = std::string("-datadir=") + dataPath.generic_string();
+
+    const char* argv[] = {
+        (char*)("/bin/test"),
+        (char*)(args.c_str())
+    };
+    pCfg->pathData = dataPath;
+    BOOST_CHECK(pCfg->Load(2, const_cast<char**>(argv), dataPath.c_str(), "bigbang.conf"));
+
+    CDocker docker;
+    InitLog(dataPath.c_str(), true, false);
+    xengine::CLog log;
+
+    BOOST_CHECK(docker.Initialize(pCfg, &log));
+    BOOST_CHECK(docker.Attach(new CConsensus()));
+    BOOST_CHECK(docker.Attach(new CCoreProtocol()));
+    auto pDummyPeerNet = new CDummyPeerNet();
+    BOOST_CHECK(docker.Attach(pDummyPeerNet));
+    BOOST_CHECK(docker.Attach(new CForkManager()));
+    BOOST_CHECK(docker.Attach(new CWallet()));
+    BOOST_CHECK(docker.Attach(new CService()));
+    BOOST_CHECK(docker.Attach(new CTxPool()));
+    BOOST_CHECK(docker.Attach(new CBlockChain()));
+    BOOST_CHECK(docker.Attach(new CBlockMaker()));
+    BOOST_CHECK(docker.Attach(new CDataStat()));
+    BOOST_CHECK(docker.Attach(new CDispatcher()));
+    BOOST_CHECK(docker.Attach(new CNetChannel()));
+    BOOST_CHECK(docker.Attach(new CDelegatedChannel()));
+
+    BOOST_CHECK(docker.Run());
+
+    std::promise<std::string> promiseIP;
+    std::future<std::string> futureIP = promiseIP.get_future();
+    auto spGetIPMsg = CPeerNetGetIPMessage::Create(promiseIP);
+    PUBLISH_MESSAGE(spGetIPMsg);
+
+    BOOST_CHECK(futureIP.get() == "0.0.0.0");
+
+    std::promise<std::size_t> promiseCount;
+    std::future<std::size_t> futureCount = promiseCount.get_future();
+    auto spGetPeerCountMsg = CPeerNetGetCountMessage::Create(promiseCount);
+    PUBLISH_MESSAGE(spGetPeerCountMsg);
+
+    BOOST_CHECK(futureCount.get() == 0);
 
     docker.Exit();
 
