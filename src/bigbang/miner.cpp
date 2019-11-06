221fcb0ed75b7f65a3affd04b1eea4fa1d266e0b
MathxH Chen <brainfvck@foxmail.com>
2019-11-06 14:40:37 +0800

    Fix bug

--- src/bigbang/miner.cpp
+++ src/bigbang/miner.cpp
@@ -83,6 +83,7 @@ bool CMiner::HandleInvoke()
     }
 
     workCurrent.hashPrev.SetHex(strHashPrev);
+    strCurrentPrevHash = strHashPrev;
 
     if (!ThreadDelayStart(thrFetcher))
     {
@@ -323,6 +324,7 @@ bool CMiner::GetWork()
         spParam->strSpent = strAddrSpent;
         spParam->strPrivkey = strMintKey;
         spParam->strPrev = workCurrent.hashPrev.GetHex();
+        std::cout << "Send GetWork Request hashPrev " << workCurrent.hashPrev.GetHex() << std::endl;
         CRPCReqPtr spReq = MakeCRPCReqPtr(nNonceGetWork, spParam);
         return SendRequest(nNonceGetWork, spReq->Serialize());
     }
