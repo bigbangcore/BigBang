2fc7caf44d8d83824df3fdcc753f0a4093ccdab2
Hantao Sun <jhkghmn@gmail.com>
2019-11-15 14:48:35 +0800

    Fix verify block Code completeness, and add an error "ERR_BLOCK_TYPE_INVALID" (#271)

--- src/bigbang/error.cpp
+++ src/bigbang/error.cpp
@@ -9,90 +9,92 @@ using namespace bigbang;
 static const char* _ErrorFailed = "operation failed";
 static const char* _ErrorUnknown = "unkown error";
 static const char* _ErrorString[] = {
-    // OK = 0,
+    // OK, 
     "",
-    // ERR_UNAVAILABLE,                           // 1
+    // ERR_UNAVAILABLE,
     "unavailable",
     /* container */
-    //ERR_NOT_FOUND,                              // 2
+    //ERR_NOT_FOUND,
     "not found",
-    //ERR_ALREADY_HAVE,                           // 3
+    //ERR_ALREADY_HAVE,
     "already have",
-    //ERR_MISSING_PREV,                           // 4
+    //ERR_MISSING_PREV,
     "missing previous",
     /* system */
-    //ERR_SYS_DATABASE_ERROR,                     // 5
+    //ERR_SYS_DATABASE_ERROR,
     "database error",
-    //ERR_SYS_OUT_OF_DISK_SPACE,                  // 6
+    //ERR_SYS_OUT_OF_DISK_SPACE,
     "out of disk space",
-    //ERR_SYS_STORAGE_ERROR,                      // 7
+    //ERR_SYS_STORAGE_ERROR,
     "storage error",
-    //ERR_SYS_OUT_OF_MEMORY,                      // 8
+    //ERR_SYS_OUT_OF_MEMORY,
     "out of memory",
     /* block */
-    //ERR_BLOCK_OVERSIZE,                         // 9
+    //ERR_BLOCK_TYPE_INVALID,
+    "block type is invalid",
+    //ERR_BLOCK_OVERSIZE,
     "block oversize",
-    //ERR_BLOCK_PROOF_OF_WORK_INVALID,            // 10
+    //ERR_BLOCK_PROOF_OF_WORK_INVALID,
     "block proof-of-work is invalid",
-    //ERR_BLOCK_PROOF_OF_STAKE_INVALID,           // 11
+    //ERR_BLOCK_PROOF_OF_STAKE_INVALID,
     "block proof-of-stake is invalid",
-    //ERR_BLOCK_TIMESTAMP_OUT_OF_RANGE,           // 12
+    //ERR_BLOCK_TIMESTAMP_OUT_OF_RANGE,
     "block timestamp is out of range",
-    //ERR_BLOCK_COINBASE_INVALID,                 // 13
+    //ERR_BLOCK_COINBASE_INVALID,
     "block coinbase is invalid",
-    //ERR_BLOCK_COINSTAKE_INVALID,                // 14
+    //ERR_BLOCK_COINSTAKE_INVALID,
     "block coinstake is invalid",
-    //ERR_BLOCK_TRANSACTIONS_INVALID,             // 15
+    //ERR_BLOCK_TRANSACTIONS_INVALID,
     "block transactions are invalid",
-    //ERR_BLOCK_DUPLICATED_TRANSACTION,           // 16
+    //ERR_BLOCK_DUPLICATED_TRANSACTION,
     "block contain duplicated transaction",
-    //ERR_BLOCK_SIGOPCOUNT_OUT_OF_BOUND,          // 17
+    //ERR_BLOCK_SIGOPCOUNT_OUT_OF_BOUND,
     "block sigopt is out of bound",
-    //ERR_BLOCK_TXHASH_MISMATCH,                  // 18
+    //ERR_BLOCK_TXHASH_MISMATCH,
     "block txid is mismatch",
-    //ERR_BLOCK_SIGNATURE_INVALID,                // 19
+    //ERR_BLOCK_SIGNATURE_INVALID,
     "block signature is invalid",
-    //ERR_BLOCK_INVALID_FORK,                     // 20
+    //ERR_BLOCK_INVALID_FORK,
     "block found invalid fork",
     /* transaction */
-    //ERR_TRANSACTION_INVALID,                    // 21
+    //ERR_TRANSACTION_INVALID,
     "transaction invalid",
-    //ERR_TRANSACTION_OVERSIZE,                   // 22
+    //ERR_TRANSACTION_OVERSIZE,
     "transaction oversize",
-    //ERR_TRANSACTION_OUTPUT_INVALID,             // 23
+    //ERR_TRANSACTION_OUTPUT_INVALID,
     "transaction outputs are invalid",
-    //ERR_TRANSACTION_INPUT_INVALID,              // 24
+    //ERR_TRANSACTION_INPUT_INVALID,
     "transaction inputs are invalid",
-    //ERR_TRANSACTION_TIMESTAMP_INVALID,          // 25
+    //ERR_TRANSACTION_TIMESTAMP_INVALID,
     "transaction timestamp is invalid",
-    //ERR_TRANSACTION_NOT_ENOUGH_FEE,             // 26
+    //ERR_TRANSACTION_NOT_ENOUGH_FEE,
     "transaction fee is not enough",
-    //ERR_TRANSACTION_STAKE_REWARD_INVALID,       // 27
+    //ERR_TRANSACTION_STAKE_REWARD_INVALID,
     "transaction stake reward is invalid",
-    //ERR_TRANSACTION_SIGNATURE_INVALID,          // 28
+    //ERR_TRANSACTION_SIGNATURE_INVALID,
     "transaction signature is invalid",
-    //ERR_TRANSACTION_CONFLICTING_INPUT,          // 29
+    //ERR_TRANSACTION_CONFLICTING_INPUT,
     "transaction inputs are conflicting",
     /* wallet */
-    //ERR_WALLET_INVALID_AMOUNT,                  // 30
+    //ERR_WALLET_INVALID_AMOUNT,
     "wallet amount is invalid",
-    //ERR_WALLET_INSUFFICIENT_FUNDS,              // 31
+    //ERR_WALLET_INSUFFICIENT_FUNDS,
     "wallet funds is insufficient",
-    //ERR_WALLET_SIGNATURE_FAILED,                // 32
+    //ERR_WALLET_SIGNATURE_FAILED,
     "wallet failed to signature",
-    //ERR_WALLET_TX_OVERSIZE,                     // 33
+    //ERR_WALLET_TX_OVERSIZE,
     "wallet transaction is oversize",
-    //ERR_WALLET_NOT_FOUND,                       // 34
+    //ERR_WALLET_NOT_FOUND,
     "wallet is missing",
-    //ERR_WALLET_IS_LOCKED,	                     // 35
+    //ERR_WALLET_IS_LOCKED,
     "wallet is locked",
-    //ERR_WALLET_IS_UNLOCKED,                     // 36
+    //ERR_WALLET_IS_UNLOCKED,
     "wallet is unlocked",
-    //ERR_WALLET_IS_ENCRYPTED,                    // 37
+    //ERR_WALLET_IS_ENCRYPTED,
     "wallet is encrypted",
-    //ERR_WALLET_IS_UNENCRYPTED,                  // 38
+    //ERR_WALLET_IS_UNENCRYPTED,
     "wallet is unencrypted",
-    //ERR_WALLET_FAILED,                          // 39
+    //ERR_WALLET_FAILED,
     "wallet operation is failed"
 };
 
