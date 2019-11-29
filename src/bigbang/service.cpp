2656ac0e1347e93c242d41c88ee7ed1727c4b39f
Loading Sun <jhkghmn@gmail.com>
2019-11-29 23:21:18 +0800

    Fix RPC block time of getwork

--- src/bigbang/service.cpp
+++ src/bigbang/service.cpp
@@ -561,7 +561,7 @@ bool CService::GetWork(vector<unsigned char>& vchWorkData, int& nPrevBlockHeight
         nPrevTime = (*it).second.nLastBlockTime;
         nPrevBlockHeight = (*it).second.nLastBlockHeight;
         block.hashPrev = hashPrev;
-        block.nTimeStamp = GetTime();//nPrevTime + BLOCK_TARGET_SPACING - 10;
+        block.nTimeStamp = max((*it).second.nLastBlockTime, GetTime());
     }
 
     nAlgo = CM_CRYPTONIGHT;
