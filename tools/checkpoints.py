b6ac165b48ad41354320fdcbc79494ac0a76a9a3
MathxH Chen <brainfvck@foxmail.com>
2019-12-27 15:43:57 +0800

    Refactor code

--- tools/checkpoints.py
+++ tools/checkpoints.py
@@ -5,8 +5,8 @@
 # This tool refer to btcd checkpoint candidate tool
 # https://github.com/bigbangcore/BigBang/issues/343
 
-import subprocess
 import json
+import subprocess
 
 # btcd confirmations is 2016, but we could reduce from 2016 to 1000 confirms,
 # cause we cannot compared with BTC' Hash Rate in the main network
@@ -16,38 +16,74 @@ def Constant_CheckPointConfirmations():
     return 1000
 
 
-# Get (last height - confirms) from best primary chain
-end_height = int(subprocess.check_output(
-    ['bigbang', 'getforkheight'])) - Constant_CheckPointConfirmations()
-
-# block at least have 1000 confirms
-height_list = [i for i in range(end_height)]
-# gensis block timestamp
-prev_block_timestamp = 1575043200
-
-print 'check point candidate list here: '
-
-for height in height_list:
+def get_block_hash(height):
     json_str = str(subprocess.check_output(
         ['bigbang', 'getblockhash', str(height)]))
 
     block_hash_list = json.loads(json_str)
     if len(block_hash_list) > 1:
-        continue
+        return False, ''
+    else:
+        return True, block_hash_list[0]
 
-    block_hash = block_hash_list[0]
 
+def get_block(block_hash):
     json_str = subprocess.check_output(['bigbang', 'getblock', block_hash])
-    block_dict = json.loads(json_str)
-    block_type = block_dict['type']
+    return json.loads(json_str)
+
+
+def get_block_type(block_dict):
+    return block_dict['type']
+
+
+def get_block_height(block_dict):
+    return block_dict['height']
+
+
+def get_block_timestamp(block_dict):
+    return block_dict['time']
+
+
+def is_primary_block(block_type):
     if "primary-" in str(block_type):
-        block_height = block_dict['height']
-        if block_height != height:
+        return True
+    else:
+        return False
+
+
+def main():
+
+    # Get (last height - confirms) from best primary chain
+    end_height = int(subprocess.check_output(
+        ['bigbang', 'getforkheight'])) - Constant_CheckPointConfirmations()
+
+    # block at least have 1000 confirms
+    height_list = [i for i in range(end_height)]
+    # gensis block timestamp
+    prev_block_timestamp = 1575043200
+
+    print 'check point candidate list here: '
+
+    for height in height_list:
+
+        is_success, block_hash = get_block_hash(height)
+        if not is_success:
             continue
-        if block_height == 0:
-            continue
-        current_block_timestamp = block_dict['time']
-        if(current_block_timestamp > prev_block_timestamp):
-            checkpoint = (block_height, block_hash)
-            print checkpoint
-            prev_block_timestamp = current_block_timestamp
+
+        block_dict = get_block(block_hash)
+        block_type = get_block_type(block_dict)
+        if is_primary_block(block_type):
+            block_height = get_block_height(block_dict)
+            if block_height != height:
+                continue
+            if block_height == 0:
+                continue
+            current_block_timestamp = get_block_timestamp(block_dict)
+            if(current_block_timestamp > prev_block_timestamp):
+                checkpoint = (block_height, block_hash)
+                print checkpoint
+                prev_block_timestamp = current_block_timestamp
+
+
+if __name__ == '__main__':
+    main()
