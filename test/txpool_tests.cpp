50b634366cce9e8d95befe67eb8269590ad0921f
MathxH Chen <brainfvck@foxmail.com>
2020-02-03 14:26:03 +0800

    Init txpool unit test

---
+++ test/txpool_tests.cpp
@@ -0,0 +1,21 @@
+// Copyright (c) 2020 The Bigbang developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "txpool.h"
+
+#include <boost/test/unit_test.hpp>
+
+#include "test_big.h"
+
+using namespace bigbang;
+
+BOOST_FIXTURE_TEST_SUITE(txpool_tests, BasicUtfSetup)
+
+BOOST_AUTO_TEST_CASE(multi_index_tx_score)
+{
+    CPooledTxLinkSet linkSet;
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
