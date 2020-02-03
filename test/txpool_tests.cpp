39cd9bac078bf01523987007179ffbb3ce3c6f62
MathxH Chen <brainfvck@foxmail.com>
2020-02-03 16:02:43 +0800

    Pass 1 case of sorted by sequence number

--- test/txpool_tests.cpp
+++ test/txpool_tests.cpp
@@ -5,6 +5,7 @@
 #include "txpool.h"
 
 #include <boost/test/unit_test.hpp>
+#include <map>
 
 #include "test_big.h"
 
@@ -15,15 +16,40 @@ BOOST_FIXTURE_TEST_SUITE(txpool_tests, BasicUtfSetup)
 BOOST_AUTO_TEST_CASE(multi_index_tx_score)
 {
     CPooledTxLinkSet setTxLinkIndex;
+    std::map<uint8, uint16> mapType{
+        {0, CTransaction::TX_TOKEN},
+        {1, CTransaction::TX_CERT},
+        {2, CTransaction::TX_GENESIS},
+        {3, CTransaction::TX_STAKE},
+        {4, CTransaction::TX_WORK}
+    };
     
-    for(int i = 0; i < 10; ++i)
+    uint32 timestamp = 1579021270;
+    for(int i = 10; i >= 0; --i)
     {
         CTransaction tx;
         tx.SetNull();
-
-        CPooledTx pooledTx(tx, -1, i++);
+        tx.nTimeStamp = timestamp++;
+        tx.nType = mapType[i % 5];
+        tx.nLockUntil = i;
+        CPooledTx pooledTx(tx, -1, i);
         setTxLinkIndex.insert(CPooledTxLink(&pooledTx));
     }
+
+    BOOST_CHECK(setTxLinkIndex.size() == 11);
+
+    const CPooledTxLinkSetBySequenceNumber& idxTxLinkSeq = setTxLinkIndex.get<1>();
+    CPooledTxLinkSetBySequenceNumber::iterator iter = idxTxLinkSeq.begin();
+
+    int k = 0;
+    for (; iter != idxTxLinkSeq.end(); ++iter)
+    {
+        BOOST_CHECK(k == iter->nSequenceNumber);
+        k++;
+    }
+    
+
+
 
 }
 
