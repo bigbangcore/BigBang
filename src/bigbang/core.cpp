d64ae9da40422735088a812df1e006d988a8885b
Hantao Sun <jhkghmn@gmail.com>
2020-04-10 16:18:52 +0800

    Fix vchData verification and DPoS template TX signature bug (#469)
    * Fix template type judgment bug when verifying TX vchData
    
    * If transfer token from DPoS template to itself should sign by owner

--- src/bigbang/core.cpp
+++ src/bigbang/core.cpp
@@ -196,7 +196,7 @@ Errno CCoreProtocol::ValidateTransaction(const CTransaction& tx)
     if (tx.nType == CTransaction::TX_TOKEN
         && (tx.sendTo.IsPubKey()
             || (tx.sendTo.IsTemplate()
-                && (tx.sendTo.GetTemplateId() == TEMPLATE_WEIGHTED || tx.sendTo.GetTemplateId() == TEMPLATE_MULTISIG)))
+                && (tx.sendTo.GetTemplateId().GetType() == TEMPLATE_WEIGHTED || tx.sendTo.GetTemplateId().GetType() == TEMPLATE_MULTISIG)))
         && !tx.vchData.empty())
     {
         if (tx.vchData.size() < 21)
