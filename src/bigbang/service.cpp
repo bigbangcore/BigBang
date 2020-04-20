d595a0934735abfb0ac9463f6cbd5fc06bf1e0fa
chenchuanhua <cch809094@163.com>
2020-04-20 14:33:05 +0800

    modify pow time and bits

--- src/bigbang/service.cpp
+++ src/bigbang/service.cpp
@@ -680,7 +680,7 @@ bool CService::GetWork(vector<unsigned char>& vchWorkData, int& nPrevBlockHeight
 
         if (pCoreProtocol->IsDposHeight(nPrevBlockHeight + 1))
         {
-            block.nTimeStamp = max(pCoreProtocol->GetNextBlockTimeStamp((*it).second.nMintType, (*it).second.nLastBlockTime, CTransaction::TX_WORK), (uint32)GetNetTime());
+            block.nTimeStamp = max(pCoreProtocol->GetNextBlockTimeStamp((*it).second.nMintType, (*it).second.nLastBlockTime, CTransaction::TX_WORK, nPrevBlockHeight + 1), (uint32)GetNetTime());
         }
         else
         {
