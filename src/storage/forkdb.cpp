d45d71ff5da839c26019dd0c7a1c00bec3335d66
MathxH Chen <brainfvck@foxmail.com>
2020-08-24 18:11:38 +0800

    Fork defi fix forkcontext (#626)
    * Refactor CForkContext
    
    * Change oldForkCtxt to newForkCtxt

--- src/storage/forkdb.cpp
+++ src/storage/forkdb.cpp
@@ -55,7 +55,22 @@ bool CForkDB::RemoveForkContext(const uint256& hashFork)
 
 bool CForkDB::RetrieveForkContext(const uint256& hashFork, CForkContext& ctxt)
 {
-    return Read(make_pair(string("ctxt"), hashFork), ctxt);
+    if(!Read(make_pair(string("ctxt"), hashFork), ctxt))
+    {
+        COldForkContext oldCtxt;
+        if(!Read(make_pair(string("ctxt"), hashFork), oldCtxt))
+        {
+            return false;
+        }
+
+        CForkContext writeNewCtxt(oldCtxt.hashFork, oldCtxt.hashJoint, oldCtxt.txidEmbedded, oldCtxt.GetProfile());
+        if(!Write(make_pair(string("ctxt"), hashFork), writeNewCtxt))
+        {
+            return false;
+        }
+    }
+    
+    return true;
 }
 
 bool CForkDB::ListForkContext(vector<CForkContext>& vForkCtxt)
